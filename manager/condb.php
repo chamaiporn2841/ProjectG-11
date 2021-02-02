780b8b7e1f3764c8099e5cd2cd90cddca1a27c4c
chamaiporn2841 <b6071011@g.sut.ac.th>
2021-02-03 02:14:01 +0700

    ลบ

--- manager/condb.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con= mysqli_connect("localhost","root","12345","benefit") or die("Error: " . mysqli_error($con));
-mysqli_query($con, "SET NAMES 'utf8' ");
-date_default_timezone_set('Asia/Bangkok');
-//devbanban.com
-
-?>
